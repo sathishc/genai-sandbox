1d139a585ffd200e203ea22f2469dacaa2d11005
sathishc <sat.hariharan@gmail.com>
2024-07-12 13:10:14 +0530

    remove old files

--- infrastructure-ts/bin/serverless-chat.ts
+++ infrastructure-ts/bin/serverless-chat.ts
@@ -4,15 +4,8 @@
 
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { WebsocketStack } from '../lib/websocket-stack';
-import { AuthenticationStack } from '../lib/auth-stack';
-import { FrontendStack } from '../lib/frontend-stack';
-import { DatabaseStack } from '../lib/database-stack';
-import { ObservabilityStack } from '../lib/observability-stack';
 import { Aspects } from 'aws-cdk-lib';
 import { AwsSolutionsChecks, NagSuppressions } from 'cdk-nag';
-import { AgentApiStack } from '../lib/agent-api-stack';
-
 import { ClaimsAgentStack } from '../lib/claims-agent-stack';
 
 /* If you don't specify 'env', this stack will be environment-agnostic.
@@ -44,53 +37,9 @@ const LOG_LEVEL = "DEBUG";
 
 const claimsAgentStack = new ClaimsAgentStack(app, 'ClaimsAgentStack', {});
 
-/*
-const websocketApiStack = new WebsocketStack(app, 'WebsocketStack', {
-  logLevel: LOG_LEVEL,
-  messagesTable: databaseStack.messagesTable,
-  agentsTable: databaseStack.agentsTable,
-  cognitoUserPoolId: authStack.cognitoUserPoolId,
-  connectionsTable: databaseStack.connectionsTable
-});
-websocketApiStack.addDependency(databaseStack);
-websocketApiStack.addDependency(authStack);
 
-
-const agentApiStack = new AgentApiStack(app, 'AgentApiStack', {
-  logLevel: LOG_LEVEL,
-  messagesTable: databaseStack.messagesTable,
-  agentsTable: databaseStack.agentsTable,
-  connectionsTable: databaseStack.connectionsTable,
-  cognitoUserPoolId: authStack.cognitoUserPoolId,
-  webSocketApi: websocketApiStack.webSocketApi
-});
-agentApiStack.addDependency(authStack);
-agentApiStack.addDependency(websocketApiStack);
-agentApiStack.addDependency(databaseStack);
-
-const observabilityStack = new ObservabilityStack(app, 'ObservabilityStack', {});
-
-
-const region = process.env.CDK_DEFAULT_REGION // this.region
-const account_id = process.env.CDK_DEFAULT_ACCOUNT //this.account
-const frontendStack = new FrontendStack(app, 'FrontendStack', {
-  restApi: agentApiStack.restApi,
-  websocketApi: websocketApiStack.webSocketApi,
-  cognitoUserPoolId: authStack.cognitoUserPoolId,
-  cognitoDomainPrefix: `gen-ai-${region}-${account_id}` // Cognito domain prefix needs to be unique globally. Please fill in your domain prefix.
-});
-frontendStack.addDependency(agentApiStack);
-*/
 
 // CDK-NAG rule supressions
-/*
-NagSuppressions.addStackSuppressions(authStack, [
-  { id: 'AwsSolutions-S1', reason: 'Server access logging disabled for sample' },
-  { id: 'AwsSolutions-S10', reason: 'Not enforcing SSL for a sample' },
-  { id: 'AwsSolutions-IAM4', reason: 'LambdaBasicExecutionRole has access to create and append to any CW log groups. Although this is not ideal, it does not pose a security risk for the sample.' },
-  { id: 'AwsSolutions-IAM5', reason: 'SMS MFA is not enabled on the Userpool.' },
-]);*/
-
 
 NagSuppressions.addStackSuppressions(claimsAgentStack, [
   { id: 'AwsSolutions-S1', reason: 'Server access logging disabled for sample' },
@@ -99,57 +48,3 @@ NagSuppressions.addStackSuppressions(claimsAgentStack, [
   { id: 'AwsSolutions-IAM5', reason: 'SMS MFA is not enabled on the Userpool.' },
   { id: 'AwsSolutions-L1', reason: 'Latest Agent Runtime suppressed for Sample' },
 ]);
-
-/*
-NagSuppressions.addStackSuppressions(agentApiStack, [
-  { id: 'AwsSolutions-APIG1', reason: 'Access logging would incur additional cost. Not required for a sample.' },
-  { id: 'AwsSolutions-APIG2', reason: 'Request validation in not mandatory in this case. It would improve the resiliency of the API, but not required for a sample.' },
-  { id: 'AwsSolutions-APIG4', reason: 'API authorization has been implemented for the non-public method.' },
-  { id: 'AwsSolutions-APIG6', reason: 'Access logging would incur additional cost. Not required for a sample.' },
-  { id: 'AwsSolutions-COG4', reason: 'Cognito authorization has been implemented for the non-public method.' },
-  { id: 'AwsSolutions-IAM4', reason: 'LambdaBasicExecutionRole has access to create and append to any CW log groups. Although this is not ideal, it does not pose a security risk for the sample.' },
-  { id: 'AwsSolutions-IAM5', reason: 'LambdaBasicExecutionRole has access to create and append to any CW log groups. Although this is not ideal, it does not pose a security risk for the sample.' },
-]);
-
-NagSuppressions.addStackSuppressions(websocketApiStack, [
-  { id: 'AwsSolutions-APIG1', reason: 'Access logging would incur additional cost. Not required for a sample.' },
-  { id: 'AwsSolutions-APIG2', reason: 'Request validation in not mandatory in this case. It would improve the resiliency of the API, but not required for a sample.' },
-  { id: 'AwsSolutions-APIG4', reason: 'API authorization has been implemented for the non-public method.' },
-  { id: 'AwsSolutions-APIG6', reason: 'Access logging would incur additional cost. Not required for a sample.' },
-  { id: 'AwsSolutions-COG4', reason: 'Cognito authorization has been implemented for the non-public method.' },
-  { id: 'AwsSolutions-IAM4', reason: 'LambdaBasicExecutionRole has access to create and append to any CW log groups. Although this is not ideal, it does not pose a security risk for the sample.' },
-  { id: 'AwsSolutions-IAM5', reason: 'LambdaBasicExecutionRole has access to create and append to any CW log groups. Although this is not ideal, it does not pose a security risk for the sample.' },
-]);
-
-
-NagSuppressions.addStackSuppressions(frontendStack, [
-  { id: 'AwsSolutions-S1', reason: "Bucket access logs are disabled by design. It would incur unnecessary cost. Only static SPA files are stored in the bucket." },
-  { id: 'AwsSolutions-S2', reason: "The bucket has public access blocked. (wrong error message?). It is only accessible via Cloudfront." },
-  { id: 'AwsSolutions-S10', reason: "SSL is enforced in a resource policy." }
-]);
-
-NagSuppressions.addResourceSuppressionsByPath(
-  frontendStack,
-  '/FrontendStack/Custom::CDKBucketDeployment8693BB64968944B69AAFB0CC9EB8756C/ServiceRole/DefaultPolicy/Resource',
-  [
-    { id: 'AwsSolutions-IAM4', reason: 'CDK managed policy - does not affect production' },
-    { id: 'AwsSolutions-IAM5', reason: 'CDK managed policy - does not affect production' }
-  ]
-);
-
-NagSuppressions.addResourceSuppressionsByPath(
-  frontendStack,
-  '/FrontendStack/Custom::CDKBucketDeployment8693BB64968944B69AAFB0CC9EB8756C/ServiceRole/Resource',
-  [
-    { id: 'AwsSolutions-IAM4', reason: 'CDK managed policy - does not affect production' },
-    { id: 'AwsSolutions-IAM5', reason: 'CDK managed policy - does not affect production' }
-  ]
-);
-
-NagSuppressions.addResourceSuppressionsByPath(
-  frontendStack,
-  '/FrontendStack/Custom::CDKBucketDeployment8693BB64968944B69AAFB0CC9EB8756C/Resource',
-  [
-    { id: 'AwsSolutions-L1', reason: 'CDK managed lambda - does not affect production' }
-  ]
-);*/
