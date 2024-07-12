9a15336884247553e1d550f3bb3e7419d67dd444
sathishc <sat.hariharan@gmail.com>
2024-07-12 17:38:24 +0530

    add auth and signout

--- chatbot-client/src/App.tsx
+++ chatbot-client/src/App.tsx
@@ -4,36 +4,60 @@ import config from './bot/config.tsx';
 import MessageParser from './bot/MessageParser.tsx';
 import ActionProvider from './bot/ActionProvider.js';
 import 'react-chatbot-kit/build/main.css'
+
 import "@cloudscape-design/global-styles/index.css"
 import Container from "@cloudscape-design/components/container";
-import Header from "@cloudscape-design/components/header";
-
+import TopNavigation from "@cloudscape-design/components/top-navigation";
 
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
 
 function App() { 
 
   return (
-    <Container
-      header={
-        <Header
-          variant="h2"
-          description="The chatbot demonstrates integration with Amazon Bedrock and Bedrock Agents"
-        >
-          Amazon Bedrock Chatbot
-        </Header>
-      }
-    >
-      <Chatbot
-        config={config}
-        messageParser={MessageParser}
-        actionProvider={ActionProvider}
+    <Authenticator>
+       {({ signOut, user }) => (
+        <div className="App">
+      <TopNavigation
+        identity={{
+          href: "#",
+          title: "Chatbot with Amazon Bedrock and Amplify",
+        }}
+        utilities={[
+          
+          {
+            type: "menu-dropdown",
+            text: "",
+            description: user?.signInDetails?.loginId,
+            iconName: "user-profile",
+            items: [
+              { id: "signout", text: "Sign out"}
+            ],
+            onItemClick: (item) => {
+              console.log(item);
+              if (item.detail.id === "signout") {
+                console.log("signing out", user);
+                //signOut();
+              }
+            }
+          }
+        ]}
       />
-    </Container>
-    
+      <Container>
+        <Chatbot
+          config={config}
+          messageParser={MessageParser}
+          actionProvider={ActionProvider}
+        />
+      </Container>
+      </div>
+       )}
+    </Authenticator>
   )
 }
 
