7f108c541d51a2a95d18ec346956f0749c93171c
sathishc <sat.hariharan@gmail.com>
2024-07-12 10:40:35 +0530

    integrate chatbot kit

---
+++ chatbot-client/src/bot/config.js
@@ -0,0 +1,8 @@
+import { createChatBotMessage } from 'react-chatbot-kit';
+
+const config = {
+  botName: "Loan Bot",
+  initialMessages: [createChatBotMessage(`Hi, I am a Loan Bot `)],
+};
+
+export default config;
